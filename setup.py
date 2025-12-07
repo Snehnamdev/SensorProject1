bf72a79ed84179459b5422193ad5ae2475f1e183
Sneh Namdev <sneh441@gmail.com>
2025-12-07 14:14:00 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    requirements= []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements= [req.replace("\n","") for req in requirements]
+        return requirements
+
+
+setup(
+name= 'Sensor Project',
+version= '0.0.1',
+author='Sneh',
+author_email ="example@gmail.com",
+install_requirements=('requirements.txt'),
+packages= find_packages()
+)
